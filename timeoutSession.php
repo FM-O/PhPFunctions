6df1d2861318b35573537d6bcf272a834e897ad3
theCrazyFox <flo69001@hotmail.fr>
2014-06-23 18:35:35 +0200

    PhP Timeout localhost function
    Function to set session time easily without to go into wamp config
    files.
    Useful to debug Session global

---
+++ timeoutSession.php
@@ -0,0 +1,22 @@
+<?php
+
+///////////////////// SECOND PART \\\\\\\\\\\\\\
+//Script to paste at the beginning of your post script.
+
+/*********** Get back the initial timestamp from the form script *******/
+
+$timeStamp = $_SESSION['clock']; // put into variable
+echo $timeStamp .'
+'; //debug counter Tstamp session form
+//Substract 10sec to the timestamp saved in the current script
+//Timestamp saved by time(), we place the value in a variable
+
+$timeStop = time()-10;
+echo $timeStop; //debug counter time current session
+
+//Test to know when we exceed the current timestamp session less 10 sec
+
+if ($timeStamp<$timeStop) {
+session_destroy();
+}
+?>
